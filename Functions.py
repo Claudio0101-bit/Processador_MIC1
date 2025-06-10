868001e067fe1ed032608a5af13c42613ce8b531
Claudio0101-bit <salgadoclaudio2005@gmail.com>
2025-06-09 21:51:41 -0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -12,15 +12,18 @@ Arquivo: Functions.py
 # String de 8 ou 12 caracteres que representa o número em Binário
 def b10_to_b2(num, bits):
 
+    '''
     # Se o número for negativo, ajusta para seu complemento em binário
     # assumindo o Sistema de Complemento de 2
     if num < 0:
         # Operador '<<' funciona como Deslocador para a Esquerda
         # (a << n) => a * 2^n
         num = (1 << bits) + num
+    '''
 
     # Converte o número para binário
     s_b2 = bin(num & (2**bits - 1))[2:].zfill(bits)
+
     '''
     Explicação do Comando:
     --- Função bin() converte o número para String binária, mas sem bits extras 
@@ -36,28 +39,20 @@ def b10_to_b2(num, bits):
     # Retorno da função
     return s_b2
 
-# Função que recebe uma String binária de 8 ou 12 caracteres
+# Função que recebe uma String binária de 8, 12 ou 16 caracteres
 # e retorna seu número decimal
 def b2_to_b10(s_b2, bits):
+
     # Converte a String para Número Decimal (como se fosse positivo)
     num = int(s_b2, 2)
 
-    # Se o bit mais à esquerda (MSB) 1, é um número negativo
+    '''
+    # Se o bit mais à esquerda (MSB) é 1, é um número negativo
     if s_b2[0] == '1':
         # Ajustando para número negativo
         num = num - (1 << bits)
+    '''
 
     return num
-
-# Testando
-print(b2_to_b10('00000101', 8))    # 5
-print(b2_to_b10('11111011', 8))    # -5
-print(b2_to_b10('000000000101', 12)) # 5
-print(b2_to_b10('111111111011', 12)) # -5
-
-
-
-
-
 
 
