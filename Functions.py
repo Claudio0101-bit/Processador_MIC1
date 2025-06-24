3a6e7d225cf3dcd9d78ddba2d18be75d904b36db
Claudio0101-bit <salgadoclaudio2005@gmail.com>
2025-06-24 19:20:37 -0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -8,20 +8,33 @@ Arquivo: Functions.py
 
 '''
 
-# Função que recebe um número decimal e retorna uma
-# String de 8 ou 12 caracteres que representa o número em Binário
-def b10_to_b2(num, bits):
+# Função que recebe uma String de números e retorna um Array deles em inteiros
+def str_to_array(str):
+    if not str.isdigit():
+        raise ValueError("String recebida em str_to_array() não contem apenas números.")
+    return [int(i) for i in str]
 
-    '''
+# Função que recebe um Array de números e retorna uma String de todos eles
+def array_to_str(arr):
+    s = ''
+    for i in arr:
+        s += str(i)
+    return s
+
+
+# Função que recebe um número decimal e retorna uma
+# Array de 8, 12 ou 16 inteiros (0 ou 1) que representa o número em Binário
+def dec_to_arraybin(num, bits):
+
     # Se o número for negativo, ajusta para seu complemento em binário
     # assumindo o Sistema de Complemento de 2
     if num < 0:
         # Operador '<<' funciona como Deslocador para a Esquerda
         # (a << n) => a * 2^n
         num = (1 << bits) + num
-    '''
 
-    # Converte o número para binário
+
+    # Converte o número para uma String Binária
     s_b2 = bin(num & (2**bits - 1))[2:].zfill(bits)
 
     '''
@@ -36,23 +49,64 @@ def b10_to_b2(num, bits):
     --- Método .zfill(N)  preenche a string com zeros à esquerda para 
     que ela tenha pelo menos N caracteres.
     '''
-    # Retorno da função
-    return s_b2
 
-# Função que recebe uma String binária de 8, 12 ou 16 caracteres
-# e retorna seu número decimal
-def b2_to_b10(s_b2, bits):
+    # Usar a String Binária para fazer o Array Binário
+    arraybin = str_to_array(s_b2)
+
+    # Retorno da função (Array Binário)
+    return arraybin
+
+# Função que recebe um Array Binário e retorna seu número decimal
+def arraybin_to_dec(arraybin):
+
+    # Usar o Array Binário para fazer a String Binária
+    s_b2 = ''
+    for i in range(len(arraybin)):
+        s_b2 += str(arraybin[i])
 
     # Converte a String para Número Decimal (como se fosse positivo)
     num = int(s_b2, 2)
 
-    '''
-    # Se o bit mais à esquerda (MSB) é 1, é um número negativo
-    if s_b2[0] == '1':
-        # Ajustando para número negativo
-        num = num - (1 << bits)
-    '''
 
+    # Se o bit mais à esquerda do Array Binário de 16 for 1,
+    # é um valor negativo de uma variável
+    if len(arraybin) == 16 and s_b2[0] == '1':
+        # Ajustando para número negativo
+        num = num - (1 << len(arraybin))
+
+    # Retorno da função (Decimal)
     return num
 
+# Função que realiza a soma da ULA (usando 2 Arrays Binários)
+def soma_ULA(a, b):
+    # Testando mesmo tamanho dos Arrays
+    if len(a) != len(b):
+        raise ValueError("Os Arrays da ULA não possuem mesmo tamanho.")
+
+    # Resultado da Soma em Array Binário
+    resultado = [0 for i in range(len(a))]
+    # Variável para guardar o Carry (+1)
+    carry = 0
+
+    # Soma bit a bit dos Arrays Binários, da direita para a esquerda
+    for i in range(len(a) - 1, -1, -1):
+        soma = a[i] + b[i] + carry  # Soma
+        resultado[i] = soma % 2  # Bit de Resultado
+        carry = soma // 2  # 'Vai um' (carry)
+
+    # Ignoramos Casos de Overflow (onde carry final = 1)
+    # Retorno do Resultado
+    return resultado
+
+# Função de Inversão da ULA
+def inv_ULA(a):
+
+    # Invertendo os bits do Array dado
+    a_inv = [1 - b for b in a]
+
+    # Somando 1 usando Função de Soma da ULA
+    mais1 = [0 for i in range(len(a))]
+    mais1[-1] = 1
+
+    return soma_ULA(a_inv, mais1)
 
